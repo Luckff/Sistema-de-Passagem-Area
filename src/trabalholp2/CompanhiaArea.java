a2e5217378fe9ab56cd3ba74b0803c1772dc30f5
Victor <silveiravictor97@gmail.com>
2015-05-19 13:51:01 -0300

    Inserido metodos para compra, consulta e cancelamento de passagens e metodos para adicionar passagens, clientes e aeronaves no arrayList

--- src/trabalholp2/CompanhiaArea.java
+++ src/trabalholp2/CompanhiaArea.java
@@ -52,6 +52,64 @@ public class CompanhiaArea {
     public void setClientes(ArrayList<Cliente> clientes) {
         this.clientes = clientes;
     }
-
-   
+    public void addCliente(Cliente cliInsert){
+        this.clientes.add(cliInsert);
+    }
+    public void addPassagem(Passagem pasInsert){
+        this.passagens.add(pasInsert);
+    }
+    public void addAeronaves(Aeronaves aerInsert){
+        this.frota.add(aerInsert);
+    }
+    public void comprarPassagemDin(Cliente clienteLogin, Voo vooEscolhido, boolean bebida, String classe,String comida){
+        int codigoPassagem = 12;
+        Passagem passagemCliente = new Passagem(codigoPassagem,bebida,comida,classe,clienteLogin,vooEscolhido);
+        addPassagem(passagemCliente);
+        clienteLogin.setPontosCartao(clienteLogin.getPontosCartao() -  vooEscolhido.getValorReal());  
+    }
+    public void comprarPassagemPon(Cliente clienteLogin, Voo vooEscolhido, boolean bebida, String classe,String comida){
+        int codigoPassagem = 13;
+        if(clienteLogin.getPontosCartao() >= vooEscolhido.getValorPontos()){
+            Passagem passagemCliente = new Passagem(codigoPassagem,bebida,comida,classe,clienteLogin,vooEscolhido);
+            addPassagem(passagemCliente);
+            clienteLogin.setPontosCartao(clienteLogin.getPontosCartao() - vooEscolhido.getValorPontos());
+        }
+        else {
+            System.out.println("Pontos Insuficientes");
+        }
+    }
+    public void cancelarPassagem(Cliente clienteLogin){
+        int j = -1,i = 0;
+        if(passagens.isEmpty()) return;
+        while(i <= passagens.size() ){
+            if(passagens.get(i).getCliente() == clienteLogin){ 
+                j = i;
+                break;
+            }
+        }
+        if(j != -1){
+            clienteLogin.setPontosCartao(clienteLogin.getPontosCartao() + passagens.get(j).getVoo().getValorPontos());
+            passagens.remove(j);
+            System.out.println("Compra cancelada.");
+        }
+    }
+    public void consultarPassagem(Cliente clienteLogin){
+        int j = -1,i = 0;
+        while(i < passagens.size() ){
+            if(passagens.get(i).getCliente() == clienteLogin){ 
+                j = i;
+                break;
+            }
+            i++;
+        }
+        if(j != -1){//precisa incluir o horario ainda
+            System.out.println("Código: "+ passagens.get(j).getCodigoPassagem());
+            System.out.println("Nome: "+ passagens.get(j).getCliente().getNome());
+            System.out.println("Vôo "+passagens.get(j).getVoo().getIdVoo()+"    Origem: "+passagens.get(j).getVoo().getCidadeOrigem()+"  Destino: "+ passagens.get(j).getVoo().getCidadeDestino());
+            System.out.println("Classe "+ passagens.get(j).getClasseDoAviao());
+        }
+        else{
+            System.out.println("Passagem não encontrada.");
+        }
+    }
 }
